8176a5100a85345008869d6c46e2935aa75f8e76
Back2Basic-T <yurieaizen01@gmail.com>
2025-09-06 09:52:42 +0800

    edit storagebucket

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAMAoqGWCMAzW3rIm8J6N0zswdnc4oxkZ4",
   authDomain: "my-first-project-7db1f.firebaseapp.com",
   projectId: "my-first-project-7db1f",
-  storageBucket: "my-first-project-7db1f.firebasestorage.app",
+  storageBucket: "my-first-project-7db1f.appspot.com",
   messagingSenderId: "926021010197",
   appId: "1:926021010197:web:dab124296890baec161f1c",
   measurementId: "G-MD4JPPTM24"
